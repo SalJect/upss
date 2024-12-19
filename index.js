85dcbcc810bdf0c92fdc2a6b927c6af41297bf51
SalJect <ghozagtps@gmail.com>
2024-12-19 11:07:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ app.all('/player/register', function(req, res) {
     res.send("Coming soon...");
 });
 app.all('/player/login/dashboard', function (req, res) {
-   const tData = {};
+    const tData = {};
     try {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
         for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
         if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
-    
+
     res.render(__dirname + '/public/html/dashboard.ejs', {data: tData});
 });
 
@@ -66,7 +66,7 @@ app.all('/player/growid/checktoken', (req, res) => {
     });
 });
 app.get('/', function (req, res) {
-   res.send('Hello');
+   res.send('Hello Memek');
 });
 
 app.listen(5000, function () {
