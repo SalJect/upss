b439ffec25a71b7dc7d4730ec8a06c4c18e91682
SalJect <ghozagtps@gmail.com>
2024-12-19 09:11:14 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Tutorial:
 ## Path Information
 
 - `/` - The main page
-- `/player/login/dashboard` - Immediately redirect to the validate path
+- `/player/login/dashboard` - The dashboard for the login modals
 - `/player/growid/login/validate` - The validation for the GrowID login
   
 ## Contact
--- index.js
+++ index.js
@@ -14,50 +14,48 @@ app.use(compression({
         return compression.filter(req, res);
     }
 }));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept',
     );
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
-app.use(function (req, res, next) {
-    console.log(req.method, req.url);
-    next();
-});
 app.use(express.json());
-
-app.post('/player/login/dashboard', (req, res) => {
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
+app.all('/player/register', function(req, res) {
+    res.send("Coming soon...");
+});
+app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
-    let encData = "";
     try {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
         for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        encData = Buffer.from(JSON.stringify(tData)).toString('base64');
-        if (uName[1] && uPass[1]) { res.status(302).redirect(`/player/growid/login/validate?token=${encData}`); }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
-    res.status(302).redirect(`/player/growid/login/validate?token=${encData}`);
+    res.render(__dirname + '/public/html/dashboard.ejs', {data: tData});
 });
 
-app.get('/player/growid/login/validate', (req, res) => {
-    const _token = req.query.token;
+app.all('/player/growid/login/validate', (req, res) => {
+    const _token = req.body._token;
     const growId = req.body.growId;
     const password = req.body.password;
 
     const token = Buffer.from(
         `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
-
+   
     res.send(
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
-
-app.post('/player/growid/checktoken', (req, res) => {
+app.all('/player/growid/checktoken', (req, res) => {
     const { refreshToken } = req.body;
     res.json({
         status: 'success',
@@ -68,7 +66,7 @@ app.post('/player/growid/checktoken', (req, res) => {
     });
 });
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+   res.send('Hello Memek');
 });
 
 app.listen(5000, function () {
