032db01d880a282bcac73ab9dd25d69a5867e420
SalJect <ghozagtps@gmail.com>
2024-12-23 19:12:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
 const express = require('express');
 const app = express();
-const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
+const rateLimiter = require('express-rate-limit');
 const compression = require('compression');
-
-function isValidEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
 
 app.use(compression({
     level: 5,
+    threshold: 0,
     filter: (req, res) => {
-        if (req.headers['X-No-Compression']) return false;
+        if (req.headers['x-no-compression']) {
+            return false;
+        }
         return compression.filter(req, res);
     }
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)} | Status: ${res.statusCode}`);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 min cd
-    max: 100, // 100 req / cd
-    message: 'Too many requests from this IP, please try again after an hour',
-}));
-app.set('trust proxy', 1);
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
-app.all('/player/login/dashboard', async function (req, res) {
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
+app.all('/player/register', function(req, res) {
+    res.send("Coming soon...");
+});
+app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n');
-        const uName = uData[0].split('|');
-        const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) {
-            const d = uData[i].split('|');
-            tData[d[0]] = d[1];
-        }
-        if (uName[1] && uPass[1]) {
-            return res.redirect('/player/growid/login/validate');
-        }
-    } catch (why) {
-        console.log(`Warning: ${why}`);
-    }
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
 
-    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
+    res.render(__dirname + '/public/html/dashboard.ejs', {data: tData});
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
@@ -60,20 +50,32 @@ app.all('/player/growid/login/validate', (req, res) => {
     const token = Buffer.from(
         `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
-
+   
     res.send(
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
-
-app.get('/', function (req, res) {
-    res.send('Growtopia Login Server');
+app.all('/player/growid/checktoken', (req, res) => {
+    const { refreshToken } = req.body;
+    try {
+    const decoded = Buffer.from(refreshToken, 'base64').toString('utf-8');
+    if (typeof decoded !== 'string' && !decoded.startsWith('growId=') && !decoded.includes('password=')) return res.render(__dirname + '/public/html/dashboard.ejs');
+    res.json({
+        status: 'success',
+        message: 'Account Validated.',
+        token: refreshToken,
+        url: '',
+        accountType: 'growtopia',
+    });
+    } catch (error) {
+        console.log("Redirecting to player login dashboard");
+        res.render(__dirname + '/public/html/dashboard.ejs');
+    }
 });
-
-app.all('/player/*', function (req, res) {
-    res.status(301).redirect('https://api.yoruakio.tech/player/' + req.path.slice(8));
+app.get('/', function (req, res) {
+   res.send('Hello Memek');
 });
 
 app.listen(5000, function () {
-    console.log(`Listening on port 5000`);
+    console.log('Listening on port 5000');
 });
